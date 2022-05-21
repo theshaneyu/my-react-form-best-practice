9650472f7e7bac603e07e84d73a821b136bed6bb
Shane Yu <shaneyu07@gmail.com>
2022-05-21 15:05:19 +0800

    WIP: add ChakraForm

--- src/components/chakra/ChakraForm.tsx
+++ src/components/chakra/ChakraForm.tsx
@@ -1,5 +1,23 @@
+import { Field, Form, Formik } from 'formik';
 import React from 'react';
 
 export const ChakraForm: React.FC = () => {
-  return <div>Chakra</div>;
+  return (
+    <>
+      <Formik
+        initialValues={{ firstName: '', lastName: '' }}
+        onSubmit={(values, actions) => {
+          console.log(values);
+          actions.resetForm();
+        }}
+      >
+        {() => (
+          <Form>
+            <Field />
+            <Field />
+          </Form>
+        )}
+      </Formik>
+    </>
+  );
 };
